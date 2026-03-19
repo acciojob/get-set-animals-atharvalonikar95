555f7551d7f3db6ae63e66c16316b5ea67e30e82
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-20 01:53:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,10 +17,11 @@ class Dog extends Animal {
 	}
 }
 
-class Cat extends Animal {}
+class Cat extends Animal {	
 	purr(){
 		console.log("purr")
 	}
+}
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
